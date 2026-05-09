58a7641de27d1c548908d21b3341a0015b551bcd
louinaquines <louinaquines@gmail.com>
2026-05-10 00:23:37 +0800

    first commit

---
+++ app/tabs/cart.tsx
@@ -0,0 +1,4 @@
+import { View, Text } from 'react-native';
+export default function Cart() {
+    return <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}><Text>Cart</Text></View>;
+}
